e43695f3e314e10db2a7b28e67c849739d2705f2
bob-jone <hdm210@gmail.com>
2024-09-12 11:21:06 -0500

    Made the camera less jittery with LateUpdate

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -9,14 +9,7 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField]
     private Vector3 _offset = new Vector3(0, 5, -7);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = _player.transform.position + _offset;
     }
