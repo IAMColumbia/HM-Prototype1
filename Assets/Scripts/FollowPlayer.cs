7ff6c54e0994ac86675a37e17c340d917e000580
bob-jone <hdm210@gmail.com>
2024-09-12 11:19:21 -0500

    Changed camera offset to a Vector3 variable

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,8 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField]
     private GameObject _player;
+    [SerializeField]
+    private Vector3 _offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = _player.transform.position + new Vector3(0, 5, -7);
+        transform.position = _player.transform.position + _offset;
     }
 }
